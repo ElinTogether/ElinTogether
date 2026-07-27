0e1d6540ea1ebaea07980f5c687ca5e2249d4bd6
gottyduke <35783662+gottyduke@users.noreply.github.com>
2026-07-27 06:24:52 -0500

    fix: client only see self button in session info layer

--- ElinTogether/Components/Tabs/TabSessionInfo.cs
+++ ElinTogether/Components/Tabs/TabSessionInfo.cs
@@ -79,7 +79,7 @@ internal class TabSessionInfo : TabEmpBase
         infoGroup.Text(BuildPingStat(player));
 
         // action buttons: only show for non-host players
-        if (player.Index != 0) {
+        if (NetSession.Instance.IsHost || player.User.IsMe) {
             var btnRow = infoGroup.Horizontal();
             btnRow.Layout.childForceExpandWidth = true;
             btnRow.Layout.childAlignment = TextAnchor.MiddleCenter;
